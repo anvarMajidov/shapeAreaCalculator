12673886ba4e3a58a029a48da216acf4ebd1544e
anvarMajidov <majidovanvar30.11.2002@gmail.com>
2022-12-13 16:03:12 +0500

    add new tests

--- TestProject/TestCircle.cs
+++ TestProject/TestCircle.cs
@@ -1,5 +1,6 @@
 using System;
 using ProjectLibrary;
+using ProjectLibrary.exceptions;
 using ProjectLibrary.shapes;
 using Xunit;
 
@@ -9,8 +10,6 @@ public class TestCircle
 {
     [Theory]
     [InlineData(10)]
-    [InlineData(11)]
-    [InlineData(2.2)]
     [InlineData(3.1)]
     public void GetArea_ValidRadius_ReturnsArea(double radius)
     {
@@ -22,6 +21,18 @@ public class TestCircle
 
         //Assert
         Assert.Equal(GetRadius(radius), area);
+    }
+    
+    [Theory]
+    [InlineData(-2)]
+    [InlineData(-12.2)]
+    public void GetArea_NegativeRadius_ReturnsException(double radius)
+    {
+        //Arrange
+        IShape shape = new Circle(radius);
+
+        //Act and Assert
+        Assert.Throws<NegativeLengthException>(() => shape.GetArea());
     }
 
     private static double GetRadius(double r) => Math.PI * r * r;
--- TestProject/TestTriangle.cs
+++ TestProject/TestTriangle.cs
@@ -1,6 +1,56 @@
-﻿namespace TestProject;
+﻿using System;
+using ProjectLibrary;
+using ProjectLibrary.exceptions;
+using ProjectLibrary.shapes;
+using Xunit;
+
+namespace TestProject;
 
 public class TestTriangle
 {
+    [Theory]
+    [InlineData(3,4,5)]
+    [InlineData(7.1,10,5.5)]
+    public void GetArea_ValidTriangle_ReturnsArea(double s1, double s2, double s3)
+    {
+        //Arrange
+        IShape triangle = new Triangle(s1, s2, s3);
+
+        //Act
+        double area = triangle.GetArea();
+
+        //Assert
+        Assert.Equal(GetArea(s1, s2, s3), area);
+    }
     
+    [Theory]
+    [InlineData(1,2,3)]
+    [InlineData(100,1,2)]
+    public void GetArea_InvalidTriangleSides_ThrowsException(double s1, double s2, double s3)
+    {
+        //Arrange
+        IShape triangle = new Triangle(s1, s2, s3);
+
+        //Act and Assert
+        Assert.Throws<InvalidTriangleException>(() => triangle.GetArea());
+    }
+    
+    [Theory]
+    [InlineData(1,-2,3)]
+    [InlineData(-100,1,2)]
+    public void GetArea_NegativeTriangleSides_ThrowsException(double s1, double s2, double s3)
+    {
+        //Arrange
+        IShape triangle = new Triangle(s1, s2, s3);
+
+        //Act and Assert
+        Assert.Throws<NegativeLengthException>(() => triangle.GetArea());
+    }
+    
+    private double GetArea(double s1, double s2, double s3)
+    {
+        double semiPerimeter = (s1 + s2 + s3) / 2;
+        double area = Math.Sqrt(semiPerimeter * (semiPerimeter - s1) * (semiPerimeter - s2) * (semiPerimeter - s3));
+        return area;
+    }
 }
